1d692b11e8b7766a6b60d6a69f698d2ef6158ad5
Kuskildin Ural <kuskildinural@gmail.com>
2025-11-04 14:38:11 +0500

    Добавил головоломку с кодом

--- SiberianJam25/Assets/Source/Scripts/Main/Level/CodePuzzle/CodePanel.cs
+++ SiberianJam25/Assets/Source/Scripts/Main/Level/CodePuzzle/CodePanel.cs
@@ -1,40 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CodePanel : MonoBehaviour
 {
     [SerializeField] private List<CodePanelButton> _butttons;
+    [SerializeField] private CodePanelButton _enterButton;
+    [SerializeField] private Text _passwordText;
+    [Header("Indicator")]
+    [SerializeField] private MeshRenderer _indicator;
+    [SerializeField] private Material _correctColor;
+    [SerializeField] private Material _incorrectColor;
+    [SerializeField] private float _indicatorShowTime = 2f;
+    [SerializeField] private AudioSource _indicatorSource;
+    [SerializeField] private AudioClip _correctCLip;
+    [SerializeField] private AudioClip _incorrectClip;
 
     private string _currentEnteredPassword = string.Empty;
     private CodeGatePuzzle _codeGate;
+    private Material _indicatorDefaultMaterial;
 
     public void Initialize(CodeGatePuzzle codeGate)
     {
+        _codeGate = codeGate;
         InitializeButtons();
+        _indicatorDefaultMaterial = _indicator.material;
     }
 
     public void EnterNewNumber(string num)
     {
+        if(num == "0")
+        {
+            CheckIsCorrectPassword();
+            return;
+        }
+
         if (_currentEnteredPassword.Length < 4)
             _currentEnteredPassword += num;
 
-        if (_currentEnteredPassword.Length >= 4)
+        if (_currentEnteredPassword.Length > 4)
             CheckIsCorrectPassword();
+
+        _passwordText.text = _currentEnteredPassword;
 
     }
 
     private void CheckIsCorrectPassword()
     {
         if(_currentEnteredPassword == GlobalVars.CodePanelCode)
-        {
-            Debug.Log("œ‡ÓÎ¸ ‚ÂÌ˚È");
+        {           
+            PlayCorrectSound();
+            ShowIndicator(true);
+            DeactivateButtons();
             _codeGate.OpenGate();
             
         }
         else
         {
-            Debug.Log("œ¿–ŒÀ‹ Õ≈ ¬≈–Õ€…!");
+            ShowIndicator(false);
+            PlayIncorrectSound();            
         }
 
         _currentEnteredPassword = string.Empty;
@@ -46,5 +71,40 @@ public class CodePanel : MonoBehaviour
         {
             button.Initialize(this);
         }
+    }
+
+    private void PlayCorrectSound()
+    {
+        _indicatorSource.PlayOneShot(_correctCLip);
+    }
+
+    private void PlayIncorrectSound()
+    {
+        _indicatorSource.PlayOneShot(_incorrectClip);
+    }
+
+    private void ShowIndicator(bool correct)
+    {
+        StartCoroutine(ShowIndicatorRoutine(correct));
+    }
+
+    private void DeactivateButtons()
+    {
+        foreach (CodePanelButton button in _butttons)
+        {
+            button.CanInteract = false;
+        }
+    }
+
+    private IEnumerator ShowIndicatorRoutine(bool correction)
+    {
+        if (correction)
+            _indicator.material = _correctColor;
+        else
+            _indicator.material = _incorrectColor;
+
+        yield return new WaitForSecondsRealtime(_indicatorShowTime);
+
+        _indicator.material = _indicatorDefaultMaterial;
     }
 }
